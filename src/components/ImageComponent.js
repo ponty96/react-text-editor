e0fb8ec1a04d5eabbf952c48929d6eb01e88e4d8
Andrew Coelho <acoelho.baa@gmail.com>
2016-03-07 11:55:14 -0800

    Initial commit

--- src/components/ImageComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Entity } from 'draft-js';
-
-export default ({ block }) => {
-  const imgContent = Entity.get(block.getEntityAt(0)).getData()['preview'];
-  return <img src={imgContent} />;
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import RichEditor from './containers/RichEditor';
-
-render(
-  <RichEditor />,
-  document.getElementById('root')
-);
