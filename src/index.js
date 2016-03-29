5b2b8b9bb436221cd8890a0b48af105e5f231a92
Aregbede Ayomide <ayo.aregbede@gmail.com>
2016-03-29 03:04:27 +0100

    export react text editor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import RichEditor from './containers/RichEditor';
 
-render(
-  <RichEditor />,
-  document.getElementById('root')
-);
+export default RichEditor;
